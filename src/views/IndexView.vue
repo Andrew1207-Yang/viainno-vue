cbb141cad6dfef975d4b36c1bedf335e5cd9283b
Andrew1207-Yang <a.yang353188@gmail.com>
2023-08-02 15:43:47 +0800

    initial push

---
+++ src/views/IndexView.vue
@@ -0,0 +1,102 @@
+<script setup>
+  import "../assets/css/index.css";
+  import Contact from "../components/Contact.vue";
+</script>
+
+<template>
+    <section class="hero">
+        <div class="h-[600px] lg:mx-auto items-center">
+            <div class="mx-auto max-w-6xl pt-[250px] text-center">
+                <h3 class="text-black text-base lg:text-lg subtitle">勤禾科技服務</h3>
+                <h1 class="text-black my-[15px] title">作業標準化建立溝通與信任</h1>
+                <p class="text-lg text-black paragraph">綜合性的營運體系來設立管理標準化、<br>建構以數據為導向的營運方式,並增加彈性自由的環境</p>
+            </div>
+        </div>
+    </section>
+
+    <!-- Home Go Section -->
+
+    <section>
+        <div class="mx-auto max-w-6xl mb-[40px]">
+            <div class="text-center mb-[60px]">
+                <h1 class="title">Home Go</h1>
+            </div>
+            <img class="mx-auto mb-[40px]" src="../assets/img/placeholder.svg" alt="">
+            <div class="flex mx-auto justify-between w-[225px]">
+                <a class="app-cta" href="#">瞭解更多</a>
+                <a class="app-cta-btn" href="#">聯絡我們</a>
+            </div>
+        </div>
+    </section>
+
+    <!-- Spaces Go Section -->
+
+    <section class="bg-black">
+        <div class="mx-auto max-w-6xl pt-[60px]">
+            <div class="text-center mb-[60px]">
+                <h1 class="title text-white"> Spaces Go </h1>
+            </div>
+            <div class="mx-auto w-[905px] h-[545px] bg-white mb-[40px]"></div>
+            <div class="flex mx-auto justify-between w-[225px] pb-[30px]">
+                <a class="app-cta" href="#">瞭解更多</a>
+                <a class="app-cta-btn" href="#">聯絡我們</a>
+            </div>
+        </div>
+    </section>
+
+    <!-- Mission Statement Section -->
+    <section class="bg-white">
+        <div class="max-w-6xl mx-auto text-center my-[60px]">
+            <h3 class="subtitle">WE FOCUS ON SPACE OPERATION AND USER EXPERIENCE</h3>
+            <h1 class="title mb-[30px]">我們專注在空間營運及使用體驗</h1>
+            <p class="mx-auto paragraph w-5/6">為了推動高度整合的智慧空間（Smart Spaces），勤禾長年投入空間及建築營運與研究，並透過自動化物件（Automated Things）和增強智慧（Augmented Intelligence）形式的人工智慧（AI），搭配物聯網（IoT）、邊緣運算（Edge Computing）和數位分身（Digital Twin）的使用，善用多種技術優勢，讓人與科技在這個開放且共同合作的連網生態內互動，致力提高人們與空間的互聯性、智能性與自主性。 <br> <br>
+                我們提供的平台解決方案（Spaces Go Platform、Home Go Platform），結合了多種元素，包括人、流程、服務和物件，創造新的應用和生活體驗，同時提供營運方方便設計及管理新的客戶服務形態。我們相信只有親身投入現場的驗證（Field Testing），才能更理解營運方的各項需求與痛點，包括資源閒置、管理不善、當前的解決方式昂貴等，在我們多年的實測，需要綜合性的營運體系來設立管理標準化、建構以數據為導向的營運方式，並增加彈性自由的環境來提高用戶的使用體驗，強化社群間的信任。
+            </p>
+        </div>
+    </section>
+    
+    <div class="max-w-7xl mx-auto xl:grid xl:grid-cols-3 text-center">
+        <div class="card">
+            <img src="../assets/img/homego/info_management.png" alt="">
+            <h3 class="subtitle">整合資訊與管理</h3>
+                <p>基礎建物資料維護與營運機制規劃，同時管理關係人、社群組識、服務廠商、聯繫資訊。解決人員異動及住戶資訊落差。
+                </p>
+        </div>
+        <div class="card">
+            <img src="../assets/img/homego/info_management.png" alt="">
+            <h3 class="subtitle">整合資訊與管理</h3>
+                <p>基礎建物資料維護與營運機制規劃，同時管理關係人、社群組識、服務廠商、聯繫資訊。解決人員異動及住戶資訊落差。
+                </p>
+        </div>
+        <div class="card">
+            <img src="../assets/img/homego/info_management.png" alt="">
+            <h3 class="subtitle">整合資訊與管理</h3>
+                <p>基礎建物資料維護與營運機制規劃，同時管理關係人、社群組識、服務廠商、聯繫資訊。解決人員異動及住戶資訊落差。
+                </p>
+        </div>
+    </div>
+
+    <!-- Tech partner Section -->
+    <section class="partner">
+        <div class="max-w-6xl text-center mx-auto my-[60px]">
+            <h1 class="title">技術夥伴</h1>
+        </div>
+    </section>
+
+    <!-- Well Building Section -->
+    <section class="partner bg-dark_green text-white">
+        <div class="max-w-6xl text-center mx-auto py-[60px]">
+            <h1 class="title">建築標章</h1>
+        </div>
+    </section>
+
+    <!-- Well Building Section -->
+    <section class="partner">
+        <div class="max-w-6xl text-center mx-auto my-[60px]">
+            <h1 class="title">合作案例</h1>
+        </div>
+    </section>
+
+    <Contact/>
+
+</template>
---
+++ src/views/SpacesGo.vue
@@ -0,0 +1,132 @@
+<script setup>
+    import "../assets/css/spacesgo.css"
+</script>
+
+<template>
+    <section class="pt-[80px] hero bg-white">
+        <div class="max-w-7xl mx-auto mt-[50px] lg:mt-[80px] text-center">
+            <div>
+                <h1 class="text-black title">Spaces Go 空間營運平台</h1>
+                <h1 class="text-black my-[15px] lg:my-[30px] subtitle">作業標準化建立溝通與信任</h1>
+            </div>
+            <img class="mx-auto mt-[50px] lg:mt-[80px]" src="../assets/img/homego/app_comparison.png" alt="product_image">
+        </div>
+    </section>
+
+    <!-- App Intro Section -->
+    <section>
+        <div class="max-w-7xl mx-auto mt-[80px] lg:mt-[180px] mb-[60px]">
+            <h1 class="title mx-5">如何彈性管理您的空間？</h1>
+            <p class="mt-[20px] mx-5">人類在經歷疫情（Covid-19）過後，企業辦公室、商務中心、旅館、各種複合型的共享工作空間等，如何有效率且安全地經營，是我們著力研究和規劃設計的問題，高度自助化、整合了人與科技的空間，又能保持人與人之間的溫度，將會是創新服務形態不可或缺的思維。</p>
+        </div>
+        <div class="max-w-7xl mx-auto xl:grid xl:grid-cols-2">
+            <div class="card">
+                <h3 class="subtitle">應用端介面直覺人性，持續地更新服務</h3>
+                <ul>
+                    <li><p>使用介面與實際作業流程相符，不刻意改變作業習慣，並且依
+                        營運不斷更新改進。
+                        </p></li>
+                    <li><p>使用介面與實際作業流程相符，不刻意改變作業習慣，並且依
+                        營運不斷更新改進。
+                        </p></li>
+                        
+                </ul>
+                <img src="../assets/img/spacesgo/stats_analysis.JPG" alt="">
+            </div>
+
+            <div class="card">
+                <h3>多元數據資料分析</h3>
+                <ul>
+                    <li><p>詳盡記錄成員針對空間、設備及活動等，從預訂、報到使用
+                        到使用完畢等不同時間點的使用情況。</p></li>
+                    <li><p>記錄各工作空間聯盟或企業及其分公司之間，空間資源共用
+                        與人員往來情況。</p></li>
+                    <li><p>從空間、設備及活動等使用情況、使用率、成員偏好使用行
+                        為，到空間能源配置與耗能情況，透過多重面向的數據，適時調
+                        整運作規則。</p></li>
+                    <li><p>可針對特定時間區間(週/月/年)的資料進行多角度歷史分析，結
+                        果可供營運決策參考。</p></li>
+                </ul>
+                <img src="../assets/img/spacesgo/phone_ui_cropped.png" alt="">
+            </div>
+
+            <div class="card">
+                <h3>多元數據資料分析</h3>
+                <ul>
+                    <li><p>詳盡記錄成員針對空間、設備及活動等，從預訂、報到使用
+                        到使用完畢等不同時間點的使用情況。</p></li>
+                    <li><p>記錄各工作空間聯盟或企業及其分公司之間，空間資源共用
+                        與人員往來情況。</p></li>
+                    <li><p>從空間、設備及活動等使用情況、使用率、成員偏好使用行
+                        為，到空間能源配置與耗能情況，透過多重面向的數據，適時調
+                        整運作規則。</p></li>
+                    <li><p>可針對特定時間區間(週/月/年)的資料進行多角度歷史分析，結
+                        果可供營運決策參考。</p></li>
+                </ul>
+                <img class="mx-auto" src="../assets/img/spacesgo/mac_dashboard.png" alt="">
+            </div>
+
+            <div class="card">
+                <h3>多元數據資料分析</h3>
+                <ul>
+                    <li><p>詳盡記錄成員針對空間、設備及活動等，從預訂、報到使用
+                        到使用完畢等不同時間點的使用情況。</p></li>
+                    <li><p>記錄各工作空間聯盟或企業及其分公司之間，空間資源共用
+                        與人員往來情況。</p></li>
+                    <li><p>從空間、設備及活動等使用情況、使用率、成員偏好使用行
+                        為，到空間能源配置與耗能情況，透過多重面向的數據，適時調
+                        整運作規則。</p></li>
+                    <li><p>可針對特定時間區間(週/月/年)的資料進行多角度歷史分析，結
+                        果可供營運決策參考。</p></li>
+                </ul>
+                <img src="../assets/img/spacesgo/pad_ui.png" alt="">
+            </div>
+        </div>
+    </section>
+
+    <!-- Product Comparison -->
+    <section class="bg-white">
+        <div class="my-[80px] max-w-6xl mx-auto text-center">
+            <div class="py-[80px]">
+                <h1 class="title">專為不同情境打造的三款軟體</h1>
+            </div>
+            <img class="mx-auto" src="../assets/img/spacesgo/app_comparison.png" alt="">
+        </div>
+    </section>
+
+    <!-- Product feature -->
+    <section class="bg-gray-200">
+        <div>
+            <div class="py-[100px] text-center mx-auto w-4xl">
+                <h3 class="text-gray-400">App Features</h3>
+                <h1 class="title mb-[20px]">Spaces Go 功能模組</h1>
+                <p>以用戶為中心設計的多項功能，解決管理上的一切需求</p>
+            </div>
+        </div>
+                    
+    </section>
+
+    <section class="bg-white">
+        <div class="max-w-6xl mx-auto mt-[30px] mb-[60px]">
+            <div class="mt-[100px] text-center mx-auto w-4xl">
+                <h3 class="text-gray-400 mb-[15px]">CREATING POSITIVE INTERACTION AND TRUST</h3>
+                <h1 class="title mb-[30px]">社區營造與友善生活的橋樑</h1>
+                <p class="hidden lg:block">都市化的過程中,人們的生活品質與建築經營密不可分,建設公司、居民委員會、物業管理<br>與居民等多方的互助信任關係,一直以來都是艱難的課題<br><br>勤禾將樓宇長期營運的作業流程標準化、建構其專有的建築/社區經營平台、提高管理與<br>決策品質。建立全面性的溝通管道,建構人、事、物的資訊中樞,讓資訊通透及知識共享。<br>透過大量可變的管理模型及策略、數位制度化管理與客觀數據、完整溝通渠道等,創造互<br>信、友善社會的企業經營目標。</p>
+                <p class="lg:hidden">都市化的過程中,人們的生活品質與建築經營密不可分,建設公司、居民委員會、物業管理與居民等多方的互助信任關係,一直以來都是艱難的課題<br><br>勤禾將樓宇長期營運的作業流程標準化、建構其專有的建築/社區經營平台、提高管理與決策品質。建立全面性的溝通管道,建構人、事、物的資訊中樞,讓資訊通透及知識共享。透過大量可變的管理模型及策略、數位制度化管理與客觀數據、完整溝通渠道等,創造互信、友善社會的企業經營目標。</p>
+            </div>
+            <img src="../assets/img/spacesgo/spacesgo_features.png" alt="">
+        </div>
+    </section>
+
+     <!-- Colab Section -->
+     <section  class="bg-gray-200">
+        <div class="max-w-6xl mx-auto pt-[30px]">
+            <div class="pt-[50px] text-center mx-auto w-4xl">
+                <h3 class="text-gray-400 pb-[15px]">EFFECTIVE CASES</h3>
+                <h1 class="title pb-[30px]">成功案例</h1>
+            </div>
+
+
+        </div>
+    </section>
+</template>
